e8b13bcee8222b16443e7e8437fefc6df37fc801
agomes02 <130376500+agomes02@users.noreply.github.com>
2024-08-21 12:20:04 -0300

    .

---
+++ js2/perguntas2.js
@@ -0,0 +1,62 @@
+const perguntas = [
+    {
+        enunciado: "principal dano causado pelo racismo?",
+        alternativas: [
+            {
+                texto: "fortalece a autoestima das vitimas",
+                afirmacao: [
+                    "racismo enfraquece a autoestima, perpetuando sentimentos de inferioridade ",
+                    "afirmacao 2"
+                    ]
+            },
+            {
+                texto: "contribui para a exclusão social e marginalização de grupos minoritarios ",
+                
+                afirmacao: [
+                    "reforça a desigualdade e estereotipos discriminatorios ",
+                    "afirmacao 2"
+                    ]
+            }           
+            
+        ]
+    },
+    {
+        enunciado: "o que caracteriza o racismo estrutural na sociedade",
+        alternativas: [
+            {
+                texto:"desigualdades raciais,
+                afirmacao: [
+                    "são perpetuadas por instituições, politicas e normas sociais ",
+                    "afirmacao 2"
+                    ]
+            },
+            {
+                texto: "discriminação individual",
+                afirmacao: [
+                    "apenas a discriminação individual e direta afeta a sociedade ",
+                    "afirmacao 2"
+                    ]
+            }
+        ]
+    },
+    {
+        enunciado: "voce apoia o racismo?",
+        alternativas: [
+            {
+                texto:"Alternativa 1 da pergunta 3",
+                afirmacao: [
+                    "afirmacao 1",
+                    "afirmacao 2"
+                    ]
+            },
+            {
+                texto:"Alternativa 2 da pergunta 3",
+                afirmacao: [
+                    "afirmacao 1",
+                    "afirmacao 2"
+                    ]
+            }
+            
+        ]
+    },
+];
