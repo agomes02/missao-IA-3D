44f93b4512bd1de2d1aa8f783c82087c3da53b92
agomes02 <130376500+agomes02@users.noreply.github.com>
2024-09-18 10:17:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,25 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
-const botaoJogarNovamente = document.querySelector(".novamente-btn");
+const botaoJogarNovamente = document.querySelector(".novamente-btn"); 
+const botaoIniciar = document.querySelector(".iniciar-btn");
+const telaInicial = document.querySelector(."tela-onicial"); 
 
 let atual = 0; 
 let perguntaAtual;
 let historiaFinal = "";
+
+botaoIniciar.addEventListener('click', iniciaJogo);
+
+function iniciaJogo() {
+    atual = 0; 
+    historiaFinal = ""; 
+    telaInicial.style.display = 'none'; 
+    caixaPerguntas.classList.remove(".nostrar");
+    caixaAlternativas.classList.remove(".nostrar");
+    caixaResultado.classList.remove(".nostrar");
+mostraPergunta(); 
+}
 
 function mostraPergunta() {
     if(atual >= perguntas.length){
@@ -60,4 +74,3 @@ function substuiNome() {
   }
 }
 substituiNomes();
-mostraPergunta();
