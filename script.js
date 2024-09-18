e00aa9d61455236bfcda7a3db417d1d9eba77f2f
agomes02 <130376500+agomes02@users.noreply.github.com>
2024-09-18 09:47:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -40,14 +40,14 @@ function respostaSelecionada(opcaoSelecionada){
 }
 
 function mostraResultado(){
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = `Em 2049,${nome}`;
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = ""; 
     CaixaResultado.classlist.ass("mostrar");
     botaoJogarNovamente.addEventListener("click", jogarNovamente);
 }
 
-function jogarNovamente{
+function jogarNovamente() {
     atual = 0;
     historiaFinal = ""; 
     caixaResultado.classLIst.remove("mostrar");
