103791b0d74105d0d753fec3038e06dffc6ef63e
agomes02 <130376500+agomes02@users.noreply.github.com>
2024-10-23 08:44:55 -0300

    Update perguntas2.js

--- js2/perguntas2.js
+++ js2/perguntas2.js
@@ -28,7 +28,6 @@ export const perguntas = [
             {
                 texto:"sim", 
                 afirmacao: [
-                    " 
                     " voce se dará melhor em humanas ",
                     
                     ]
